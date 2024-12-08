ddf135ec95fc6900c718c18b96c10dd6cfa997da
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-12-08 01:05:38 +0100

    adjustments

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -5,8 +5,9 @@
 export default function HomePage () {
 
   return (
-    <section>
-      <div>
+    <section className={`mt-10`}>
+      <img src="images/logo1-removebg-preview.png" alt="" className={`mx-auto`}/>
+      <div className={`p-5 flex justify-center items-center`}>
 
       </div>
     </section>
