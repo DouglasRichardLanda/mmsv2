fd609540b6082aedf12192d3e53ac27a89faa6d6
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-12-15 12:53:45 +0100

    footer element finished and for all resolutions adjusted

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -13,10 +13,10 @@ export default function Footer () {
 
   return (
     <footer id={`footer`} className={`bg-gray-300 mx-auto`}>
-      <div className={`w-2/3 mx-auto text-black py-20 flex gap-20 justify-between tracking-wider`}>
-        <div className={`w-1/3`}>
+      <div className={`w-2/3 mx-auto text-black py-5 lg:py-20 flex gap-5 lg:gap-20 flex-col lg:flex-row justify-between tracking-wider`}>
+        <div className={`w-full lg:w-1/3`}>
           <h2 className={`text-3xl font-extralight`}>Stay tuned</h2>
-          <div className={`mt-20`}>
+          <div className={`mt-5 lg:mt-20`}>
             Thanks for stopping by! We're always excited to hear from our fans and fellow rock enthusiasts. Drop us a message, and let’s keep the energy alive—your voice is what fuels our music!
           </div>
           <div className={`mt-10`}>
@@ -32,11 +32,11 @@ export default function Footer () {
             <button className={`border-b-2 border-gray-500 hover:border-red-600 duration-200 font-bold`}>Press</button>
           </div>
         </div>
-        <div className={`w-2/3`}>
+        <div className={`w-full lg:w-2/3 mt-10 lg:mt-0`}>
           <div>
             <h2 className={`text-3xl font-extralight`}>Contact Us</h2>
             <form>
-              <div className={`my-5 flex gap-10`}>
+              <div className={`my-5 flex gap-10 flex-col md:flex-row`}>
                 <label className={`flex flex-col gap-3 text-gray-600`}>
                   First Name
                   <input type="text" placeholder={`Martin`} className={`focus:outline-0 bg-gray-300 border-b-2 border-black pb-2 text-red-600`}/>
@@ -46,7 +46,7 @@ export default function Footer () {
                   <input type="text" placeholder={`Johnson`} className={`focus:outline-0 bg-gray-300 border-b-2 border-black pb-2 text-red-600`}/>
                 </label>
               </div>
-              <div className={`flex items-end gap-5`}>
+              <div className={`flex flex-col md:flex-row md:items-end gap-5`}>
                 <textarea rows={5} className={`w-full bg-gray-300 border-b-2 border-black focus:outline-0`} placeholder={`Hi, \n\n I would like to...`} />
                 <button onClick={() => {
                   alert("Thank you for your message. We will keep in touch as soons a possible!")
@@ -57,7 +57,7 @@ export default function Footer () {
           <div className={`mt-20`}>
             <h2 className={`text-3xl font-extralight`}>Subscribe</h2>
             <p className={`my-5 text-gray-600`}>Sing up for the mailing list to get all of the latest news and updates!</p>
-            <div className={`flex gap-5`}>
+            <div className={`flex gap-5 flex-col md:flex-row`}>
               <input type="text" placeholder={`youremail@gmail.com`} className={`focus:outline-0 bg-gray-300 border-b-2 border-black pb-2 w-full text-red-600`}/>
               <button className={`px-5 py-2 bg-red-600 text-white`}>Subscribe</button>
             </div>
