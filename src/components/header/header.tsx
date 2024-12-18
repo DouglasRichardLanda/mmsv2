e47db1d9bb94ffa13f35eca30db9ab9c7451fb53
RichardWalterLanda <richardwalterlanda@gmail.com>
2024-12-18 10:48:19 +0100

    header adjusted. Music page Layout adjusted

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,21 +7,39 @@ import {HiMenuAlt2} from "react-icons/hi";
 
 export default function Header() {
 
+  const location = useLocation();
+  const [scrollY, setScrollY] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(false)
+
   const scroll_to_footer = () => {
     const footer = document.getElementById("footer")
     footer?.scrollIntoView({behavior: 'smooth'})
     setOpen(false)
   }
 
-  const location = useLocation();
-  const [open, setOpen] = useState<boolean>(false)
+  useEffect(() => {
+    const logo = document.getElementById("main-logo")
+    window.onload = () => {
+      if(logo) {
+        logo.classList.remove("opacity-0");
+        logo.classList.add("opacity-100");
+      }
+    }
+    addEventListener("scroll", () => {
+      setScrollY(window.scrollY)
+    })
 
+    return () => {
+      console.log(window.scrollY)
+    }
+  }, []);
   useEffect(() => {
     setOpen(false)
   }, [location]);
 
   return (
     <>
+      {/*Desktop navigation*/}
       <header className={`fixed top-0 w-full lg:border-b-2 border-gray-500 bg-black/50 hidden md:block`}>
         <nav
           className={`w-full lg:w-[70%] mx-auto p-2 lg:border-x-2 border-gray-500 flex justify-around text-white uppercase text-lg xl:text-3xl xl2:text-4xl font-light`}>
@@ -41,15 +59,23 @@ export default function Header() {
              className={`fixed right-0 xl:right-10 -top-8 w-[200px] hover:animate-ping hidden lg:block`}/>
       </header>
 
+      {/*Main Logo img*/}
+      <img id={"main-logo"} src="images/logo1-removebg-preview.png" alt="MadMan'sSpirit"
+           className={merge(`hidden xs:block duration-700 fixed top-12 md:top-20 left-[40%] md:left-1/2 -translate-x-1/2 transition-all ease-in-out`, scrollY === 0 ? "w-[70%] md:w-[60%]" : "w-[70%] md:w-1/4")} />
+
+      {/*Burger button*/}
       <button
-        className={`fixed top-10 right-10 z-[1000] md:hidden`}
+        className={`fixed top-10 right-5 z-[1000] md:hidden`}
         onClick={() => {
-        setOpen(prev => !prev)
-      }}>
-        <HiMenuAlt2 className={merge(`w-20 h-20 duration-300`, open ? "rotate-90 text-gray-800" : "rotate-0 text-white")} />
+          setOpen(prev => !prev)
+        }}>
+        <HiMenuAlt2
+          className={merge(`w-14 h-14 duration-300`, open ? "rotate-90 text-gray-800" : "rotate-0 text-white")}/>
       </button>
 
-      <header className={merge(`w-full fixed bg-gray-300 top-0 h-full py-5 duration-300 md:hidden`, open ? "top-0": "-top-[150%]")}>
+      {/*Small devices navigation*/}
+      <header
+        className={merge(`w-full fixed bg-gray-300 top-0 h-full py-5 duration-300 md:hidden`, open ? "top-0" : "-top-[150%]")}>
         <nav
           className={`w-full mx-auto p-2 flex flex-col justify-center uppercase text-2xl font-bold text-gray-800 gap-1`}>
           <Link to={`/`} className={`hover:text-red-600 duration-200 p-3 text-center`}>Home</Link>
